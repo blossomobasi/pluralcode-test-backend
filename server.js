91ade16b54a22396f16ce18668b766b8259b5996
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-09 15:47:10 +0100

    refactor: Update server.js for better structure and database connection

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const app = require("./index");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 
-const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
+const DB = process.env.DATABASE_URL.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(() => {
+    console.log("Database connection successful...");
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`);
 });
