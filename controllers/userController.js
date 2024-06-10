b2c72be3207bd92c5fdb5c19c05702244133fffd
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-10 11:28:43 +0100

    Wrap the catchAsync function in the user handler operations and add friendly error message for CastError in production mode

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,77 +1,61 @@
 const User = require("../models/userModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
 
-const getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find();
+const getAllUsers = catchAsync(async (req, res, next) => {
+    const users = await User.find();
 
-        res.status(200).json({ status: "success", result: users.length, users });
-    } catch (error) {
-        res.status(500).json({ status: "failed", message: error.message });
-    }
-};
+    res.status(200).json({ status: "success", result: users.length, users });
+});
 
-const getUserById = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) return res.status(404).json({ status: "failed", message: "User not found" });
+const getUserById = catchAsync(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
 
-        res.status(200).json({ status: "success", user });
-    } catch (error) {
-        res.status(500).json({ status: "failed", message: error.message });
-    }
-};
+    if (!user) return next(new AppError("User with the given ID was not found!", 404));
 
-const createUser = async (req, res) => {
-    if (!req.body) return res.status(400).json({ message: "Please provide user details" });
+    res.status(200).json({ status: "success", user });
+});
 
-    try {
-        const { firstName, lastName, email, password } = req.body;
-        const user = await User.create({
+const createUser = catchAsync(async (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
+    const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        password,
+    });
+
+    res.status(200).json({ status: "success", user });
+});
+
+const updateUser = catchAsync(async (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
+
+    const updatedUser = await User.findOneAndUpdate(
+        req.params.id,
+        {
             firstName,
             lastName,
             email,
             password,
-        });
+        },
+        {
+            new: true,
+            runValidators: true,
+        }
+    );
 
-        res.status(200).json({ status: "success", user });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-};
+    if (!updatedUser) return next(new AppError("User with the given ID was not found!", 404));
 
-const updateUser = async (req, res) => {
-    try {
-        if (!req.body) return res.status(400).json({ message: "Please provide new user details" });
-        const { firstName, lastName, email, password } = req.body;
+    res.status(200).json({ status: "success", updatedUser });
+});
 
-        const updatedUser = await User.findOneAndUpdate(
-            req.params.id,
-            {
-                firstName,
-                lastName,
-                email,
-                password,
-            },
-            {
-                new: true,
-                runValidators: true,
-            }
-        );
+const deleteUser = catchAsync(async (req, res, next) => {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return next(new AppError("User with the given ID was not found!", 404));
 
-        res.status(200).json({ status: "success", updatedUser });
-    } catch (err) {
-        res.status(500).json({ status: "failed", message: err.message });
-    }
-};
-const deleteUser = async (req, res) => {
-    try {
-        await User.findByIdAndDelete(req.params.id);
-
-        res.status(204).json({ status: "success", message: "User deleted successfully" });
-    } catch (err) {
-        res.status(500).json({ status: "failed", message: err.message });
-    }
-};
+    res.status(204).json({ status: "success", message: "User deleted successfully" });
+});
 
 // Export all at once
 module.exports = {
