51e7b3bb129d94d6899d724cc8ae17466d05bb10
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-12 15:22:23 +0100

    update: Add a  /me route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
 
+router.get("/me", authController.protect, userController.getMe);
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.route("/").get(userController.getAllUsers).post(userController.createUser);
